3376b6974f79876c151d8a92bd78cbd7c522f525
HAMZA AIT OUMGHAR <hamwac5@yahoo.fr>
2018-08-22 22:34:21 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+    defined('_JEXEC') or die('Restricted Access'); 
+?> 
+<!DOCTYPE html> 
+<html xmlns=”http://www.w3.org/1999/xhtml" xml:lang=”en” lang=”en”> 
+<head> 
+ <title>Android Warehouse</title> 
+</head> 
+<body> 
+ <h1>Welcome to Android Warehouse!</h1> 
+ <p>We supply a large range of Androids, of all shapes and sizes, for a variety of functions.</p> 
+</body> 
+</html>
